10dedeb0b56111c0c0219d7129c75d6e490a4101
AirLi1507 <air220917@proton.me>
2025-03-04 13:32:23 +0800

    re-building of RadioCard component (WIP)

--- src/Dashboard/Components/Radio.tsx
+++ src/Dashboard/Components/Radio.tsx
@@ -13,57 +13,13 @@ interface RadioCardItemProps {
 }
 
 const RadioCard = {
-  Group: forwardRef<HTMLDivElement, RadioCardGroupProps>((props, ref) => {
-    return (
-      <div onClick={(e: React.MouseEvent)=>{
-        console.log(RadioCard.Value)
-      }}>
-        {props.children}
-      </div>
-    )
-  }),
   Item: ({value, children, defaultClass, checkedClass, uncheckedClass}: RadioCardItemProps) => {
-
-    const [cardChecked, checkCard] = useState(false)
-
-    function setCardState() {
-      if (RadioCard.Value === value) {
-        checkCard(true)
-      } else {
-        checkCard(false)
-      }
-    }
-
-    useEffect(()=>{
-      setCardState()
-      document.addEventListener("click",()=>{
-        setCardState()
-      })
-    })
-    
     return (
-      <div
-        className={`
-          radioCard
-          select-none
-          ${defaultClass}
-          ${
-            cardChecked
-            ?
-            checkedClass
-            :
-            uncheckedClass
-          }
-        `}
-        id={value}
-        onClick={(e: React.MouseEvent)=>{
-          RadioCard.Value = value
-        }}>
-        {children}
-      </div>
+      <label>
+        <input type="radio" />
+      </label>
     )
-  },
-  Value: ""
+  }
 }
 
 
